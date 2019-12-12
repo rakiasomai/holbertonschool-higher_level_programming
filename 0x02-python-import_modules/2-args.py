6dcda4efcd233a46986d3f87d68c36f67e6ad0b1
somairakia <somai.rakia@hotmail.fr>
2019-12-12 16:41:13 +0000

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    total = len(sys.argv)
-    if total <= 1:
-        print("0 argument.")
+    if (len(sys.argv)) <= 1:
+        print("0 arguments.")
     else:
-        if total == 2:
-            print("{:d} argument:".format(total - 1))
+        if (len(sys.argv)) == 2:
+            print("{:d} argument:".format((len(sys.argv)) - 1))
         else:
-            print("{:d} arguments:".format(total - 1))
-        for i in range(1, total):
-            print("{:d}: {}".format(i, sys.argv[i]))
+            print("{:d} arguments:".format((len(sys.argv)) - 1))
+        for x in range(1, (len(sys.argv))):
+            print("{:d}: {}".format(x, sys.argv[x]))
