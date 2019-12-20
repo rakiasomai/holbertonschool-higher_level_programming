a0293466df6ef6f278c5fb71929d32111b197939
somairakia <somai.rakia@hotmail.fr>
2019-12-20 08:04:42 +0000

    commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def complex_delete(my_dict, value):
-    copy = my_dict.copy()
+def complex_delete(a_dictionary, value):
+    copy = my_dictionary.copy()
     for y, z in copy.items():
         if value in z:
-            del my_dict[y]
-    return my_dict
+            del a_dictionary[y]
+    return a_dictionary
