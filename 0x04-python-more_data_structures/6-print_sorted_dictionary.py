365d701a68995a5e07acaf9a4a95312b2fb60bbf
somairakia <somai.rakia@hotmail.fr>
2019-12-20 08:00:12 +0000

    commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(my_dict):
-    for y in sorted(my_dict.keys()):
-        print("{}: {}".format(y, my_dict[y]))
+def print_sorted_dictionary(a_dictionary):
+    for y in sorted(a_dictionary.keys()):
+        print("{}: {}".format(y, a_dictionary[y]))
