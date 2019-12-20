a31e4d684b68f25c4591c7be1eea63343545a9f1
somairakia <somai.rakia@hotmail.fr>
2019-12-20 07:54:29 +0000

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    return max(my_dict, key=my_dict.get) if my_dict else None
+def best_score(a_dictionary):
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
