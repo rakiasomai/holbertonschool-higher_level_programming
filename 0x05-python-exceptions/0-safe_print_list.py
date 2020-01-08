167a4d6b923bdd8db45576391a52038755b9881e
somairakia <somai.rakia@hotmail.fr>
2020-01-08 16:45:04 +0000

    commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for y in range(z):
+            print("{}".format(my_list[y]), end="")
+    except IndexError:
+        return y
+    else:
+        return z
+    finally:
+        print("")
