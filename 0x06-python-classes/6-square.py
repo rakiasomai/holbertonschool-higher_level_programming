beb4d8d89fd761cea4e62127f65f3317f4553655
somairakia <somai.rakia@hotmail.fr>
2020-01-09 20:55:26 +0000

    commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
