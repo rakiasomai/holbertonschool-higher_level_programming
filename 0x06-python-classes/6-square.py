ded99539dcaff564b5dcf8c8b12235817135ab6c
somairakia <somai.rakia@hotmail.fr>
2020-01-09 20:43:13 +0000

    commit

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
