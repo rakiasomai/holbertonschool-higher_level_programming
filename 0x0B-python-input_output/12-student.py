6b3e23fc70e40e292428b9efd020918e527e3bb1
somairakia <somai.rakia@hotmail.fr>
2020-01-23 09:24:09 +0000

    commit

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {rslt: value for rslt, value in self.__dict__.items()
+                    if rslt in attrs}
