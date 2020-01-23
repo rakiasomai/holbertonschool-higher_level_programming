5ed2d9f89fe09049b3ee388a11179774b4250270
somairakia <somai.rakia@hotmail.fr>
2020-01-23 08:48:23 +0000

    fix

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-class Student:
+"""
+module of class student
+"""
 
+
+class Student:
+    """
+    class student
+    """
     def __init__(self, first_name, last_name, age):
+        """
+        initialization
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_jason(self):
+    def to_json(self):
+        """
+        return dictionary representation
+        """
         return (self.__dict__)
