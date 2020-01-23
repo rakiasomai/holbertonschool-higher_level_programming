7a2080aa4ee21299e178932b4dfdb676eb9de666
somairakia <somai.rakia@hotmail.fr>
2020-01-23 09:03:12 +0000

    fox

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
+
 def read_lines(filename="", nb_lines=0):
-    with open(filename, encoding="utf-8") as fd:
-        if (nb_lines == 0):
-            print(fd.read(), end="")
-        else:
-            for lines in fd:
-                if (nb_lines > 0):
-                    print(line, end="")
-                    nb_lines -= 1
+    numb = 0
+    with open(filename, encoding='utf-8') as fd:
+        for lines in fd:
+            numb = numb + 1
+            print(lines, end="")
+            if nb_lines == numb:
+                break
