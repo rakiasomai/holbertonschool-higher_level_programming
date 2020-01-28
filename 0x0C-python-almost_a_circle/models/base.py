8869e149ed8778c3674aaf413f1bc40ce2a7c604
somairakia <somai.rakia@hotmail.fr>
2020-01-28 16:15:25 +0000

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,39 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''jason function'''
+        if list_dictionaries is None:
+            return '[]'
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''save function'''
+        new_list = []
+        filename = cls.__name__ + ".json"
+        if list_objs is not None:
+            for j in list_objs:
+                new_list.append(j.to_dictionary())
+        with open(filename, 'w') as fd:
+            fd.write(cls.to_json_string(new_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''from jason str function'''
+
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''it's the create function'''
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
