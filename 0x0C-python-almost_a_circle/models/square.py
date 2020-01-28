aa5a6e984d1674167229917d2d8fb3034ba72655
somairakia <somai.rakia@hotmail.fr>
2020-01-28 17:03:51 +0000

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         '''def init'''
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
