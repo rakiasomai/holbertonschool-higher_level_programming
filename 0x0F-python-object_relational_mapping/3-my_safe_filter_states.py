5a7b4bb4d35673c26eabb37e69a80e721122735a
rakiasomai <1069@holbertonschool.com>
2020-03-29 14:20:34 +0000

    task3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Print all STATES '''
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    datab = MySQLdb.connect(user=sys.argv[1],
+                            passwd=sys.argv[2],
+                            db=sys.argv[3],
+                            host='localhost',
+                            port=3306)
+    cur = datab.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    allStates = cur.fetchall()
+
+    for state in allStates:
+        if state [1] == sys.argv[4]:
+            print(state)
+
+    cur.close()
+    datab.close()
