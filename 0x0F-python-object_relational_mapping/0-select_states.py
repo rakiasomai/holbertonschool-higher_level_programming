ba54c6dccfafceadc6abe219e9ff34f5fffd41d7
rakiasomai <1069@holbertonschool.com>
2020-03-29 13:08:25 +0000

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     import sys
 
     datab = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
+                            passwd=sys.argv[2],
+                            db=sys.argv[3],
+                            host='localhost',
+                            port=3306)
     cur = datab.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     allStates = cur.fetchall()
