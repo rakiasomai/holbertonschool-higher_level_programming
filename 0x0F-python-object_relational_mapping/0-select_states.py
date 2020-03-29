6bf94ad43222a3650eb2e158936aa52aceb37494
rakiasomai <1069@holbertonschool.com>
2020-03-29 12:56:43 +0000

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          host='localhost',
                          port=3306)
     cur = datab.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     allStates = cur.fetchall()
 
     for state in allStates:
