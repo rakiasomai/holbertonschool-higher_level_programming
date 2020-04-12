1174a13ebbb0b10437a81b4ebcdef89350506779
rakiasomai <1069@holbertonschool.com>
2020-04-12 15:51:05 +0000

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that makes a request that causes the server to respond with a message
-curl -sL -X PUT "You got me!" -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+#script that makes a request that causes the server to respond with a message
+curl -sL 0:5000/catch_me -X PUT "You got me!" -H "Origin: HolbertonSchool" -d "user_id=98"
