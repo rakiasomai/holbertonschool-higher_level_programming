254c80d8d9a099fde3dcbb2a1ef31631e5025b6d
rakiasomai <1069@holbertonschool.com>
2020-04-16 15:16:36 +0000

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,11 +3,10 @@
 script that takes in a URL, sends a request to the URL
 and displays the value of the variable
 '''
-
 import sys
 import requests
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    r = requests.get(url)
+    urll = sys.argv[1]
+    r = requests.get(urll)
     print(r.headers['x-request-id'])
