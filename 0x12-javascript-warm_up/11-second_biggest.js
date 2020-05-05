fb37f714871f5026db46c2a5b295d05d30a12b75
rakiasomai <1069@holbertonschool.com>
2020-05-05 18:29:18 +0000

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-let args = process.argv.slice(2).map((x) => {
-  return parseInt(x);
-});
-if (args.length <= 1) {
-  console.log(0);
-} else {
-  console.log(args.sort((a, b) => {
-    return b - a;
-  })[1]);
+let args = 0;
+const m = process.argv.slice(2);
+if (m.length > 1) {
+  m.sort((x, y) => x - y);
+  args = m[m.length - 2];
 }
+console.log(args);
