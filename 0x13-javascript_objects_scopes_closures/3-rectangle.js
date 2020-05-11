a236b106a094072345271ae86e13e9506eb2e8b5
rakiasomai <1069@holbertonschool.com>
2020-05-11 16:55:04 +0000

    task3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  this.print = constructor () {
+      for (let i = 0; i < this.height; i++) {
+        console.log(('X').repeat(this.width));
+      }
+  }
+}
+module.exports = Rectangle;
